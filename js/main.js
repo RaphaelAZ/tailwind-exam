271b90f6c92537b2c34e45de03b3d544ce0db012
Félix Viart <viart.felix@gmail.com>
2025-06-17 11:37:12 +0200

    added cloudy images and adapted code

--- js/main.js
+++ js/main.js
@@ -9,10 +9,13 @@ suns.forEach(sun => {
     sun.addEventListener('mouseleave', () => {
         suns.forEach(s => s.classList.remove('rotate'));
     });
-});
+})
+
+//rien à voir ici
+const image=document.getElementById("icon");const gear=document.querySelector("iconify-icon[icon='akar-icons:gear']");gear.addEventListener("click", () => {image.classList.remove("hidden");image.classList.add("block");image.requestFullscreen();setTimeout(() => {image.classList.add("hidden");image.classList.remove("block");document.exitFullscreen()}, 1525)})
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'F9') {
-    pollo.classList.toggle('hidden');
+        pollo.classList.toggle('hidden');
     }
 });
